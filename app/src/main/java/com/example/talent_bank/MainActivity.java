42efe1995f6d3b5f607f82d8e9a14410a5b95919
CHENEY-0314 <529269793@qq.com>
2020-04-04 16:12:51 +0800

    version2.2

--- app/src/main/java/com/example/talent_bank/MainActivity.java
+++ app/src/main/java/com/example/talent_bank/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -76,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
         mViewPagerAdapterForNav = new ViewPagerAdapterForNav(getSupportFragmentManager());
         viewPager.setAdapter(mViewPagerAdapterForNav);
         mViewPagerAdapterForNav.setFragment(mFragments);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        bottomNavigationView.setItemTextAppearanceInactive(R.style.bottom_normal_text);
     }
 
     private void initElement() {
@@ -113,13 +115,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private static int getStatusBarHeight(Context context) {
-        int result = 0;
-        int resourceId = context.getResources().getIdentifier(
-                "status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
+
+
 }
