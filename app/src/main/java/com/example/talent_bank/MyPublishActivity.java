8bd147be08b9c02cfc1cef943a3fd1e59d663f01
CHENEY-0314 <529269793@qq.com>
2020-04-04 22:34:43 +0800

    version2.5

--- app/src/main/java/com/example/talent_bank/MyPublishActivity.java
+++ app/src/main/java/com/example/talent_bank/MyPublishActivity.java
@@ -2,13 +2,35 @@ package com.example.talent_bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class MyPublishActivity extends AppCompatActivity {
 
+    private ImageView imgBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //设置屏幕上方状态栏颜色
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            Window window = getWindow();
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//状态栏字体颜色设置为黑色这个是Android 6.0才出现的属性
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_publish);
+
+        imgBack=findViewById(R.id.MPimg_back);
+
+        imgBack.setOnClickListener(new View.OnClickListener() {  //点击返回按钮返回上一页面
+            @Override
+            public void onClick(View v) {  //点击上方返回按钮
+                MyPublishActivity.this.finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/talent_bank/home_page/HomeFragment.java
+++ app/src/main/java/com/example/talent_bank/home_page/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.talent_bank.home_page;
 
 import androidx.lifecycle.ViewModelProviders;
+
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,14 +10,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.example.talent_bank.MyApplyActivity;
+import com.example.talent_bank.MyBiographicalActivity;
+import com.example.talent_bank.MyCollectionActivity;
+import com.example.talent_bank.MyPublishActivity;
+import com.example.talent_bank.SetUpActivity;
 import com.example.talent_bank.viewmodel.HomeViewModel;
 import com.example.talent_bank.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel mViewModel;
+    private View mView;
+    private ImageView mSetUp;
     private LinearLayout mMyPublish;
     private LinearLayout mMyApply;
     private LinearLayout mMyCollection;
@@ -28,7 +38,9 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        mView=inflater.inflate(R.layout.home_fragment, container, false);
+        initView();
+        return mView;
     }
 
     @Override
@@ -36,6 +48,46 @@ public class HomeFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void initView() {
+        mMyPublish=mView.findViewById(R.id.home_btn_MyPublish);
+        mMyPublish.setOnClickListener(new ButtonListener());
+        mMyApply=mView.findViewById(R.id.home_btn_MyApply);
+        mMyApply.setOnClickListener(new ButtonListener());
+        mMyCollection=mView.findViewById(R.id.home_btn_MyCollection);
+        mMyCollection.setOnClickListener(new ButtonListener());
+        mMyBiographical=mView.findViewById(R.id.home_btn_MyBiographical);
+        mMyBiographical.setOnClickListener(new ButtonListener());
+        mSetUp=mView.findViewById(R.id.home_setup);
+        mSetUp.setOnClickListener(new ButtonListener());
+    }
+
+    private class ButtonListener implements View.OnClickListener {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.home_btn_MyPublish:
+                    Intent intent= new Intent(getActivity(), MyPublishActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.home_btn_MyApply:
+                    Intent intent1= new Intent(getActivity(), MyApplyActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.home_btn_MyCollection:
+                    Intent intent2= new Intent(getActivity(), MyCollectionActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.home_btn_MyBiographical:
+                    Intent intent3= new Intent(getActivity(), MyBiographicalActivity.class);
+                    startActivity(intent3);
+                    break;
+                case R.id.home_setup:
+                    Intent intent4= new Intent(getActivity(), SetUpActivity.class);
+                    startActivity(intent4);
+                    break;
+            }
+        }
     }
 
 }
