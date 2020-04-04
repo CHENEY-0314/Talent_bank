a089e4194539493f8e035d0fb8f4c02e6f59c6a3
CHENEY-0314 <529269793@qq.com>
2020-04-04 22:35:34 +0800

    version2.6

---
+++ app/src/main/java/com/example/talent_bank/SetUpActivity.java
@@ -0,0 +1,34 @@
+package com.example.talent_bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+public class SetUpActivity extends AppCompatActivity {
+
+    private ImageView imgBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //设置屏幕上方状态栏颜色
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            Window window = getWindow();
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//状态栏字体颜色设置为黑色这个是Android 6.0才出现的属性
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up);
+
+        imgBack=findViewById(R.id.Setup_back);
+
+        imgBack.setOnClickListener(new View.OnClickListener() {  //点击返回按钮返回上一页面
+            @Override
+            public void onClick(View v) {  //点击上方返回按钮
+                SetUpActivity.this.finish();
+            }
+        });
+    }
+}
