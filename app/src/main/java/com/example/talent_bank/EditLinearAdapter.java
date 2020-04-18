ba4c54e6c9a9bcf4a0368d51a3efbc0504daf56c
EcoilGo <979999139@qq.com>
2020-04-18 23:34:15 +0800

    2020-04-18

---
+++ app/src/main/java/com/example/talent_bank/EditLinearAdapter.java
@@ -0,0 +1,60 @@
+package com.example.talent_bank;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Paint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EditLinearAdapter extends RecyclerView.Adapter<EditLinearAdapter.LinearViewHolder> {
+    private String shpName = "SHP_NAME";
+    private EditPeopleDemand mContext;
+
+    public EditLinearAdapter (EditPeopleDemand context) {
+        this.mContext = context;
+    }
+    @NonNull
+    @Override
+    public EditLinearAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.rv_epd_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EditLinearAdapter.LinearViewHolder holder, int position) {
+        holder.textView.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);  //给textView设置下划线
+        holder.textView.setOnClickListener(new View.OnClickListener() {  //点击返回按钮返回上一页面
+            @Override
+            public void onClick(View v) {  //点击编辑按钮
+                Intent intent = new Intent(mContext,EditDemand.class);
+                mContext.startActivity(intent);
+            }
+        });
+        SharedPreferences shp = mContext.getApplication().getSharedPreferences(shpName, Context.MODE_PRIVATE);
+        String x =shp.getString("editDemand_key","");
+        holder.textContent.setText(x);
+    }
+
+    @Override
+    public int getItemCount() {
+        SharedPreferences shp = mContext.getSharedPreferences(shpName, Context.MODE_PRIVATE);
+        int x = shp.getInt("editNum_key",0);
+        return x;
+    }
+
+    class LinearViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView,textContent;
+
+        public LinearViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.epd_item_textview);
+            textContent = itemView.findViewById(R.id.epd_item_content);
+        }
+    }
+
+}
--- app/src/main/java/com/example/talent_bank/viewmodel/GuideViewModel.java
+++ app/src/main/java/com/example/talent_bank/viewmodel/GuideViewModel.java
@@ -10,7 +10,11 @@ import androidx.lifecycle.MutableLiveData;
 
 public class GuideViewModel extends AndroidViewModel {
     private MutableLiveData<Boolean> fristload;
+    private MutableLiveData<Integer> editnum;
+    private MutableLiveData<String> editDemand;
     private String key = "fristload_key";
+    private String key2 = "editNum_key";
+    private String key3 = "editDemand_key";
     private String shpName = "SHP_NAME";
 
     public GuideViewModel(@NonNull Application application) {
@@ -19,6 +23,16 @@ public class GuideViewModel extends AndroidViewModel {
             fristload = new MutableLiveData<>();
             SharedPreferences shp = getApplication().getSharedPreferences(shpName, Context.MODE_PRIVATE);
             fristload.setValue(shp.getBoolean(key,true));
+        }
+        if(editnum == null) {
+            editnum = new MutableLiveData<>();
+            SharedPreferences shp = getApplication().getSharedPreferences(shpName, Context.MODE_PRIVATE);
+            editnum.setValue(shp.getInt(key2,0));
+        }
+        if(editDemand == null) {
+            editDemand = new MutableLiveData<>();
+            SharedPreferences shp = getApplication().getSharedPreferences(shpName, Context.MODE_PRIVATE);
+            editDemand.setValue(shp.getString(key3,""));
         }
     }
 }
