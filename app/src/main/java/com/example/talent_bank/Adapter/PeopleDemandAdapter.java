e2fa9f40dfae9eeaad642d541f73308cb3da07b3
EcoilGo <979999139@qq.com>
2020-04-29 16:59:43 +0800

    2020-04-29

--- app/src/main/java/com/example/talent_bank/Adapter/PeopleDemandAdapter.java
+++ app/src/main/java/com/example/talent_bank/Adapter/PeopleDemandAdapter.java
@@ -1,9 +1,12 @@
 package com.example.talent_bank.Adapter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,8 +17,14 @@ import com.example.talent_bank.R;
 
 import cn.refactor.lib.colordialog.ColorDialog;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class PeopleDemandAdapter extends RecyclerView.Adapter<PeopleDemandAdapter.LinearViewHolder> {
     private PeopleDemand mContext;
+
+    //以下用于手机存用户信息
+    private SharedPreferences AllProjectData;
+    private SharedPreferences.Editor AllProjectDataEditor;
 
     public PeopleDemandAdapter (PeopleDemand Context) {
         this.mContext = Context;
@@ -29,6 +38,24 @@ public class PeopleDemandAdapter extends RecyclerView.Adapter<PeopleDemandAdapte
 
     @Override
     public void onBindViewHolder(@NonNull PeopleDemandAdapter.LinearViewHolder holder, int position) {
+        AllProjectData = mContext.getSharedPreferences("all_project_data",MODE_PRIVATE);
+        String member_title = AllProjectData.getString("member_title","");
+        String member_tag = AllProjectData.getString("member_tag","");
+        String remark = AllProjectData.getString("remark","");
+
+        String[] member_titlestrarr = member_title.split("~");
+        String[] remarkstrarr = remark.split("~");
+        String[] member_tagstrarr = member_tag.split("~");
+
+        //将各个项目名字写上
+        for(int m=0;m<member_titlestrarr.length;m++) {
+            if(position==m) {
+                holder.textName.setText(member_titlestrarr[m]);
+                holder.textContent.setText(remarkstrarr[m]);
+                holder.textTag.setText(member_tagstrarr[m]);
+            }
+        }
+
         holder.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,14 +85,22 @@ public class PeopleDemandAdapter extends RecyclerView.Adapter<PeopleDemandAdapte
 
     @Override
     public int getItemCount() {
-        return 3;
+        SharedPreferences shp = mContext.getSharedPreferences("all_project_data", Context.MODE_PRIVATE);
+        int m=shp.getInt("curr_pj",-1);
+        String member_count=shp.getString("count_member","");
+        String[] count_memberstrarr = member_count.split("~");
+        return Integer.parseInt(count_memberstrarr[m]);
     }
 
     class LinearViewHolder extends RecyclerView.ViewHolder {
         private Button button;
+        TextView textName,textTag,textContent;
         public LinearViewHolder(@NonNull View itemView) {
             super(itemView);
             button = itemView.findViewById(R.id.pd_item_apply);
+            textName = itemView.findViewById(R.id.epd_item_name);
+            textTag = itemView.findViewById(R.id.epd_item_tag);
+            textContent = itemView.findViewById(R.id.epd_item_content);
         }
     }
 }
--- app/src/main/java/com/example/talent_bank/PeopleDemand.java
+++ app/src/main/java/com/example/talent_bank/PeopleDemand.java
@@ -4,18 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.talent_bank.Adapter.EditLinearAdapter;
 import com.example.talent_bank.Adapter.PeopleDemandAdapter;
 import com.example.talent_bank.Adapter.ReceiveApplyAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PeopleDemand extends AppCompatActivity {
     private ImageView imgBack;
     private RecyclerView mRvMain;
+    private LinearLayout runWebView;
+
+    //以下用于手机存用户信息
+    private SharedPreferences AllProjectData;
+    private SharedPreferences.Editor AllProjectDataEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +52,117 @@ public class PeopleDemand extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people_demand);
 
-        imgBack=findViewById(R.id.PDimg_back);
+        runWebView = findViewById(R.id.people_demand_loading);
+        imgBack = findViewById(R.id.PDimg_back);
         mRvMain = findViewById(R.id.rv_people_demand);
+
+        AllProjectData = getSharedPreferences("all_project_data",MODE_PRIVATE);
+        AllProjectDataEditor = AllProjectData.edit();
+        showProgress(true);
+        loadingProject();
 
         imgBack.setOnClickListener(new View.OnClickListener() {  //点击返回按钮返回上一页面
             @Override
             public void onClick(View v) {  //点击上方返回按钮
+                AllProjectDataEditor.putString("remark","");
+                AllProjectDataEditor.putString("member_title","");
+                AllProjectDataEditor.putString("member_tag","");
                 PeopleDemand.this.finish();
             }
         });
-        mRvMain.setLayoutManager(new LinearLayoutManager(PeopleDemand.this));
-        mRvMain.setAdapter(new PeopleDemandAdapter(PeopleDemand.this));
+
     }
+
+
+
+    /**
+     * Shows the progress UI and hides the login form.
+     * 显示进度UI并隐藏登录表单。
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        //在Honeycomb MR2上，我们有ViewPropertyAnimator API，可以实现非常简单的动画。如果可用，请使用这些API淡入进度微调器。
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        mRvMain.setVisibility(!show ? View.VISIBLE : View.GONE);
+        runWebView.setVisibility(show ? View.VISIBLE : View.GONE);
+        runWebView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                runWebView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+        mRvMain.animate().setDuration(shortAnimTime).alpha(
+                !show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mRvMain.setVisibility(!show ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    //加载当前项目需求成员的基本信息到手机暂存
+    public void loadingProject(){
+
+        final String ALLpj_id = AllProjectData.getString("pj_id","");
+        int Curr_pj = AllProjectData.getInt("curr_pj",-1);
+        String[] ALLpj_idstrarr = ALLpj_id.split("~");
+        final String pj_id = ALLpj_idstrarr[Curr_pj];
+        //请求地址
+        String url = "http://47.107.125.44:8080/Talent_bank/servlet/GetProjectMemberByID?pj_id="+pj_id;
+        String tag = "GetProjectMember";
+        //取得请求队列
+        RequestQueue GetProjectMember = Volley.newRequestQueue(this);
+        //防止重复请求，所以先取消tag标识的请求队列
+        GetProjectMember.cancelAll(tag);
+        //创建StringRequest，定义字符串请求的请求方式为POST(省略第一个参数会默认为GET方式)
+        final StringRequest GetProjectMemberrequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = (JSONObject) new JSONObject(response).get("1");
+                            AllProjectDataEditor.putString("remark",jsonObject.getString("remark"));
+                            AllProjectDataEditor.putString("member_title",jsonObject.getString("member_title"));
+                            AllProjectDataEditor.putString("member_tag",jsonObject.getString("member_tag"));
+                            AllProjectDataEditor.apply();
+                            Handler mHandler = new Handler();
+                            mHandler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    showProgress(false);
+                                    mRvMain.setLayoutManager(new LinearLayoutManager(PeopleDemand.this));
+                                    mRvMain.setAdapter(new PeopleDemandAdapter(PeopleDemand.this));
+                                }
+                            },400);
+                        } catch (JSONException e) {
+                            //做自己的请求异常操作，如Toast提示（“无网络连接”等）
+                            Toast.makeText(PeopleDemand.this,"无网络连接！",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //做自己的响应错误操作，如Toast提示（“请稍后重试”等）
+                Toast.makeText(PeopleDemand.this,"请稍后重试！",Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams()  {
+                Map<String, String> params = new HashMap<>();
+                params.put("pj_id", pj_id);
+                return params;
+            }
+        };
+        //设置Tag标签
+        GetProjectMemberrequest.setTag(tag);
+        //将请求添加到队列中
+        GetProjectMember.add(GetProjectMemberrequest);
+
+    }
+
+
 }
